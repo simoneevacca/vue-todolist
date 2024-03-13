042668827b3e9e7e70b84958f99273e1cee7eb34
simoneevacca <v.simone.vacca@gmail.com>
2024-03-13 17:38:54 +0100

    fix delete function and add input for add new task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,53 @@
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        
-        
-        toDoList: [
-            {
-                text: 'todo 1',
-                done: false,
-            },
-            {
-                text: 'todo 2',
-                done: true,
-            },
-            {
-                text: 'todo 3',
-                done: false,
-            },
-        ]
+createApp({
+  data() {
+    return {
+
+      newTask: '',
+
+      toDoList: [
 
 
-      }
+        {
+          text: 'todo 1',
+          done: false,
+        },
+        {
+          text: 'todo 2',
+          done: true,
+        },
+        {
+          text: 'todo 3',
+          done: false,
+        },
+      ]
 
+
+    }
+
+  },
+
+  methods: {
+    clickItem(index) {
+      this.toDoList[index].done = !this.toDoList[index].done
+      console.log(this.toDoList[index].done)
     },
 
-    methods:{
-        clickItem(index){
-            this.toDoList[index].done = !this.toDoList[index].done
-            console.log(this.toDoList[index].done)
-        },
-        
-        delete(index){
-            console.log(this.toDoList[index].done)
-
-        },
-    },
-
-    mounted() {
+    deleteItem(index) {
       console.log(this.toDoList)
+      this.toDoList.splice(index, 1)
+
     },
-  }).mount('#app')
+
+    addButton() {
+      this.toDoList.push({ text: this.newTask, done: false })
+      console.log(this.toDoList)
+      this.newTask = ''
+    }
+  },
+
+  mounted() {
+    console.log(this.toDoList)
+  },
+}).mount('#app')
